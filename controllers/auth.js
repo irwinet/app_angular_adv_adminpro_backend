071b3ca03105cfa2924dc3631dc5e5b2fde68869
irwinet <irwinestradatorres@gmail.com>
2021-10-31 00:20:31 -0500

    Implement Menu

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,6 +3,7 @@ const Usuario = require('../models/usuario');
 const bcrypt = require('bcryptjs');
 const { generarJWT } = require('../helpers/jwt');
 const { googleVerify } = require('../helpers/google-verify');
+const { getMenuFrontEnd } = require('../helpers/menu-frontend');
 
 const login = async (req, res = response) => {
 
@@ -34,7 +35,8 @@ const login = async (req, res = response) => {
 
         res.json({
             ok: 'true',
-            token
+            token,
+            menu: getMenuFrontEnd(usuarioDB.role)
         });
     } catch (error) {
         console.log(error);
@@ -79,7 +81,8 @@ const googleSignin = async (req, res = response) => {
 
         res.json({
             ok: true,
-            token
+            token,
+            menu: getMenuFrontEnd(usuario.role)
         });
     } catch (error) {
         res.status(401).json({
@@ -101,7 +104,8 @@ const renewToken = async (req, res = response) => {
     res.json({
         ok: true,
         token,
-        usuario
+        usuario,
+        menu: getMenuFrontEnd(usuario.role)
     });
 }
 
